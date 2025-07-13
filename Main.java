83da573598e88123ebe698383d1c4fb3cfba9a81
Giano-CS <gianosammartino2024@gmail.com>
2025-07-13 03:40:12 +0000

    Part 2 done

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to CS 112!");
+
+        Main.printIntroduction();
+
+        System.out.println("Do I like football? " + Main.isAlike("football"));
+
+    }
+
+
+    public static void printIntroduction() {
+        System.out.println(
+                "Hello, my name is Giano. I like soccer, skateboarding, snowboarding, videogames, and technology.");
+
+    }
+    
+
+    public static boolean isAlike(String topic) {
+        String myLikes = "I like soccer, skateboarding, snowboarding, videogames, and technology.";
+
+        return myLikes.contains(topic);
+
+        
     }
 }
